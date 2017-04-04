935b5abd48c84e7416ec3cf28ff99e50da3f81d6
Fredy Benavides <f.benavides@uniandes.edu.co>
2017-04-04 12:59:42 -0500

    ini 2

--- src/main/java/edu/uniandes/ecos/simpson/app/App.java
+++ src/main/java/edu/uniandes/ecos/simpson/app/App.java
@@ -1,5 +1,17 @@
 package edu.uniandes.ecos.simpson.app;
 
+import spark.ModelAndView;
+import spark.template.freemarker.FreeMarkerEngine;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.*;
+
 /**
  * Hello world!
  *
@@ -8,6 +20,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	port(Integer.valueOf(System.getenv("PORT")));
+        staticFileLocation("/public");
+    	get("/hello", (req, res) -> "Hello World");
     }
 }
